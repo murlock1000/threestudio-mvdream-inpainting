31b7b4893880d0e9387fb447d4b22bf94543df37
Alfie Macintosh <alfie@malcolm.ad.dcs.gla.ac.uk>
2023-12-13 22:01:07 +0000

    Inpainting

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 from .background import neural_environment_map_background
-from .data import uncond_multiview
-from .guidance import mvdream_guidance
-from .system import mvdream
+from .data import uncond_multiview, cond_multiview
+from .guidance import mvdream_guidance, mvdream_inpaint_guidance
+from .system import mvdream, mvdreamInpaint
